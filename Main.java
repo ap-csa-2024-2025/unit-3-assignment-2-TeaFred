8045ce120df8d0ffcd726567769aabb75ab334c0
TeaFred <fhtruong100@student.hbuhsd.edu>
2024-10-29 11:20:26 -0700

    safasf

--- Main.java
+++ Main.java
@@ -4,6 +4,12 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter a double: ");
+    double num1 = nextDouble();
+
+    if(num1 == 12.345){
+      System.out.println("YES");
+    }
   }
 }
